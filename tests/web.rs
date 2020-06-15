c3fad4baae1f05673ceb48c069f2534232f4ac8f
Daniel Boyle <boylede+github@gmail.com>
2020-06-14 22:22:33 -0700

    cargo fmt

--- tests/web.rs
+++ tests/web.rs
@@ -27,7 +27,7 @@ pub fn input_spaceship() -> Universe {
     let mut universe = Universe::new();
     universe.set_width(6);
     universe.set_height(6);
-    universe.set_cells_alive(&[(1,2), (2,3), (3,1), (3,2), (3,3)]);
+    universe.set_cells_alive(&[(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)]);
     universe
 }
 
@@ -36,6 +36,6 @@ pub fn expected_spaceship() -> Universe {
     let mut universe = Universe::new();
     universe.set_width(6);
     universe.set_height(6);
-    universe.set_cells_alive(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
+    universe.set_cells_alive(&[(2, 1), (2, 3), (3, 2), (3, 3), (4, 2)]);
     universe
 }
